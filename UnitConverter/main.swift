7937735558981d68e58a0b6ce6ea85c4c7d41b96
dayeong <sdy2856@gmail.com>
2019-12-17 21:15:10 +0900

    길이변환-출력

--- UnitConverter/main.swift
+++ UnitConverter/main.swift
@@ -8,5 +8,16 @@
 
 import Foundation
 
-print("Hello, World!")
 
+func convertToMeter(centi: Double) {
+    let meter = centi / 100
+    print("\(meter)m")
+}
+
+func converToCenti(meter: Double) {
+    let centi = Int(meter * 100)
+    print("\(centi)cm")
+}
+
+convertToMeter(centi: 120)
+converToCenti(meter: 1.86)
