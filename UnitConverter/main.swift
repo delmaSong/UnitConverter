c78839fccdca6e3ce80798c6fe8b0819163a87ba
dayeong <sdy2856@gmail.com>
2019-12-18 23:36:55 +0900

    인치 길이 변환 기능 구현

--- UnitConverter/main.swift
+++ UnitConverter/main.swift
@@ -9,39 +9,81 @@
 import Foundation
 
 
-func convertToMeter(centi: Double) {
-    let meter = centi / 100
-    print("\(meter)m")
-}
-
-func converToCenti(meter: Double) {
-    let centi = Int(meter * 100)
-    print("\(centi)cm")
-}
-
-convertToMeter(centi: 120)
-converToCenti(meter: 1.86)
-
-func convert(unit: String){
-    if unit.contains("cm"){
-       let end = unit.index(unit.endIndex, offsetBy: -2)
-        let range = unit.startIndex..<end
-        var newUnit = Double(unit[range])
-        newUnit = newUnit! / 100
-        print("\(newUnit!)m")
-    }else if unit.contains("m"){
-       let end = unit.index(unit.endIndex, offsetBy: -1)
-        let range = unit.startIndex..<end
-        var newUnit = Double(unit[range])
-        newUnit = newUnit! * 100
-        print("\(newUnit!)cm")
+//입력값 공백 유무 확인
+func startConvert(){
+   let inputValue = readLine()
+    if inputValue!.contains(" "){
+        setStart(inputValue: inputValue!)
     }else{
-        print("정확한 값을 입력해주세요")
+        checkStartUnit(startUnit: inputValue!)
+    }
+           
+}
+
+//공백 존재시 시작유닛, 타겟 유닛 설정
+func setStart(inputValue: String){
+    let targets = inputValue.split(separator: " ")
+    let startUnit = String(targets[0])
+    let targetUnit = String(targets[1])
+        
+    if startUnit.contains("cm"), targetUnit.contains("inch"){
+        centiToInch(startUnit: startUnit)
+    }else if startUnit.contains("m"), targetUnit.contains("inch"){
+        centiToInch(startUnit: meterToCenti(startUnit: startUnit))
+    }else if startUnit.contains("inch"), targetUnit.contains("m"){
+        centiToMeter(startUnit: inchToCenti(startUnit: startUnit))
+    }else{
+        checkStartUnit(startUnit: startUnit)
     }
     
 }
-convert(unit: "183cm")
-convert(unit: "3.14m")
 
-let inputValue = readLine()
-convert(unit: inputValue ?? "0")
+//시작유닛 확인
+func checkStartUnit(startUnit: String){
+    if startUnit.contains("cm"){
+        centiToMeter(startUnit: startUnit)
+    }else if startUnit.contains("m"){
+       meterToCenti(startUnit: startUnit)
+    }else if startUnit.contains("inch"){
+        inchToCenti(startUnit: startUnit)
+    }else{
+        print("지원하지 않는 단위입니다. 다시 입력해주세요")
+        startConvert()
+    }
+}
+
+func centiToMeter(startUnit: String){
+    let end = startUnit.index(startUnit.endIndex, offsetBy: -2)
+    let range = startUnit.startIndex..<end
+    var newUnit = Double(startUnit[range])
+    newUnit = newUnit! / 100
+    print("\(newUnit!)m")
+}
+
+func meterToCenti(startUnit: String) -> String{
+    let end = startUnit.index(startUnit.endIndex, offsetBy: -1)
+    let range = startUnit.startIndex..<end
+    var newUnit = Double(startUnit[range])
+    newUnit = newUnit! * 100
+    print("\(newUnit!)cm")
+    return "\(newUnit!)"
+}
+
+func inchToCenti(startUnit: String) -> String{
+   let end = startUnit.index(startUnit.endIndex, offsetBy: -4)
+   let range = startUnit.startIndex..<end
+   var newUnit = Double(startUnit[range])
+   newUnit = newUnit! * 2.54
+   print("\(newUnit!)cm")
+    return "\(newUnit!)"
+}
+
+func centiToInch(startUnit: String){
+   let end = startUnit.index(startUnit.endIndex, offsetBy: -2)
+   let range = startUnit.startIndex..<end
+   var newUnit = Double(startUnit[range])
+   newUnit = newUnit! / 2.54
+   print("\(newUnit!)inch")
+}
+
+startConvert()
